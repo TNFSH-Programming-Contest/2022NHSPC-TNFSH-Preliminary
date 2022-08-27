696f4b46839f376b1e0c94e7af21dc9b6164d9d7
rurutoria7 <nandokabeam@gmail.com>
2022-08-28 07:00:36 +0800

    add pD

--- pD/gen/gen.cpp
+++ pD/gen/gen.cpp
@@ -1,14 +1,78 @@
-#include <bits/stdc++.h>
 #include "testlib.h"
+#include <bits/stdc++.h>
+#define rep(i,j,k) for (int i=j; i<=k; i++)
+#define de(x) cout << #x << "=" << x << ", "
+#define dd cout << '\n';
+#define ff first
+#define ss second
+#define pb push_back
+#define lyx ios::sync_with_stdio(0), cin.tie(0);
+#define endl '\n'
+using namespace std;
+typedef pair<int,int> pii;
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int mnN;
+int mxN;
+int mnM;
+int mxM;
+int treeRatio;
+int tentRatio;
+
+const int N = 20;
+int n, m, rreq[N], creq[N];
+char G[N][N];
+int has_sol;
+
+int can(int i, int j)
+{
+    if (G[i][j] == 'T')
+        return 0;
+    if (G[i+1][j]=='A'||G[i][j+1]=='A'||G[i-1][j]=='A'||G[i][j-1]=='A')
+        return 0;
+    if (G[i+1][j]!='T'&&G[i][j+1]!='T'&&G[i-1][j]!='T'&&G[i][j-1]!='T')
+        return 0;
+    return 1;
+}
+
+signed main(signed argc, char* argv[]) {
 	registerGen(argc, argv, 1);
-	// about testlib, see https://codeforces.com/blog/entry/18291
+	mnN = atoll(argv[1]);
+	mxN = atoll(argv[2]);
+	mnM = atoll(argv[3]);
+	mxM = atoll(argv[4]);
+	treeRatio = atoll(argv[5]);
+	tentRatio = atoll(argv[6]);
 
-	// TODO
-	int n = atoi(argv[1]);
-	cout << n << endl;
+	n = rnd.wnext(mnN, mxN, 2);
+	m = rnd.wnext(mnN, mxN, 2);
+	has_sol = rnd.next(1,100) <= 80;
 
+	rep(i,1,n) rep(j,1,m) G[i][j] = '.';
+
+	rep(i,1,n)
+		rep(j,1,m)
+			if (rnd.next(1,100) <= treeRatio)
+				G[i][j] = 'T';
+
+	rep(i,1,n)
+		rep(j,1,m)
+			if (can(i,j) && rnd.next(1, 100) <= tentRatio)
+				G[i][j] = 'A', rreq[i]++, creq[j]++;
+
+	if (!has_sol)
+	{
+		rep(i,1,n) rreq[i] = rnd.wnext(i, 1, m, 3);
+		rep(i,1,m) creq[i] = rnd.wnext(i, 1, n, 3);
+	}
+	
+	cout << n << ' ' << m << '\n';
+	rep(i,1,n) cout << rreq[i] << "\n "[i+1 <= n];
+	rep(i,1,m) cout << creq[i] << "\n "[i+1 <= m];
+	rep(i,1,n)
+	{
+		rep(j,1,m) cout << (G[i][j]=='T'? 'T':'.');
+		cout << endl;
+	}
 	return 0;
 }
--- pD/solution/correct.cpp
+++ pD/solution/correct.cpp
@@ -1,10 +1,86 @@
+// simple search
+// rr7
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	// TODO
-	int n;
-	cin >> n;
-	cout << 0 << endl;
-	return 0;
+const int N = 15;
+
+int n, m, rreq[N], creq[N], rcnt[N], ccnt[N], has_sol;
+char G[N][N];
+
+int legal(int i, int j)
+{
+    if (j == m && rreq[i] != rcnt[i])
+        return 0;
+    if (i == n && creq[j] != ccnt[j])
+        return 0;
+    return 1;
+}
+
+int can(int i, int j)
+{
+    if (G[i][j] == 'T')
+        return 0;
+    if (G[i+1][j]=='A'||G[i][j+1]=='A'||G[i-1][j]=='A'||G[i][j-1]=='A')
+        return 0;
+    if (G[i+1][j]!='T'&&G[i][j+1]!='T'&&G[i-1][j]!='T'&&G[i][j-1]!='T')
+        return 0;
+    return 1;
+}
+
+void dfs(int i, int j)
+{
+    if (has_sol) return;
+
+    if (i==n+1 && j==1)
+    {
+        has_sol = 1;
+        for (int i=1; i<=n; i++)
+        {
+            for (int j=1; j<=m; j++)
+            {
+                cout << G[i][j];
+            }
+            cout << endl;
+        }
+        exit(0);
+    }
+    else
+    {
+        int x = (i-1)*m+j+1, ni = (x-1)/m+1, nj = (x%m==0? m : x%m);
+
+        if (legal(i, j)) dfs(ni, nj);
+        if (can(i,j))
+        {
+            char tmp = 'A';
+            swap(G[i][j], tmp);
+            rcnt[i]++;
+            ccnt[j]++;
+            if (legal(i, j)) dfs(ni, nj);
+            swap(G[i][j], tmp);
+            rcnt[i]--;
+            ccnt[j]--;
+        }
+    }
+}
+
+int main()
+{
+    cin >> n >> m;
+    for (int i=1; i<=n; i++) cin >> rreq[i];
+    for (int i=1; i<=m; i++) cin >> creq[i];
+    for (int i=1; i<=n; i++)
+    {
+        for (int j=1; j<=m; j++)
+        {
+            cin >> G[i][j];
+        }
+    }
+
+    // dfs
+    dfs(1,1);
+    if (!has_sol)
+    {
+        cout << "QwQ\n";
+    }
 }
