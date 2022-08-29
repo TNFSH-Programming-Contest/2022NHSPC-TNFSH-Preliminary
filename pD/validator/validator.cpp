0967d693b1e3672bcd900480ee2b91e71030db36
Xi-Plus <huangxuanyuxiplus@gmail.com>
2022-08-29 16:10:43 +0800

    pD Add validator

--- pD/validator/validator.cpp
+++ pD/validator/validator.cpp
@@ -1,3 +1,4 @@
+// By xiplus
 #include "testlib.h"
 using namespace std;
 
@@ -7,9 +8,37 @@ int main() {
 	registerValidation();
 	// about testlib, see https://codeforces.com/blog/entry/18291
 
-	// TODO
-	inf.readInt(1, MAXN, "n");
+	int n, m;
+	n = inf.readInt();
+	inf.readSpace();
+	m = inf.readInt();
 	inf.readEoln();
+
+	for (int q = 0; q < n; q++) {
+		if (q > 0) {
+			inf.readSpace();
+		}
+		inf.readInt(0, m);
+	}
+	inf.readEoln();
+
+	for (int q = 0; q < m; q++) {
+		if (q > 0) {
+			inf.readSpace();
+		}
+		inf.readInt(0, n);
+	}
+	inf.readEoln();
+
+	char c;
+	for (int q = 0; q < n; q++) {
+		for (int w = 0; w < m; w++) {
+			c = inf.readChar();
+			ensuref(c == '.' || c == 'T', "except '.' or 'T', got %c.", c);
+		}
+		inf.readEoln();
+	}
+
 	inf.readEof();
 
 	return 0;
