6c3cec0340af60f6486efe011e16da099f79c168
Xi-Plus <huangxuanyuxiplus@gmail.com>
2022-09-05 21:31:56 +0800

    pD Fix checker and Fix 2 hack tests

--- pD/checker/checker.cpp
+++ pD/checker/checker.cpp
@@ -12,26 +12,26 @@ int main(int argc, char* argv[]) {
 	m = inf.readInt();
 	inf.readEoln();
 
-	int rcnt[n], ccnt[m];
-	for (int q = 0; q < n; q++) {
-		if (q > 0) {
+	int rcnt[n + 1], ccnt[m + 1];
+	for (int q = 1; q <= n; q++) {
+		if (q > 1) {
 			inf.readSpace();
 		}
 		rcnt[q] = inf.readInt();
 	}
 	inf.readEoln();
 
-	for (int q = 0; q < m; q++) {
-		if (q > 0) {
+	for (int q = 1; q <= m; q++) {
+		if (q > 1) {
 			inf.readSpace();
 		}
 		ccnt[q] = inf.readInt();
 	}
 	inf.readEoln();
 
-	char itable[n][m];
-	for (int q = 0; q < n; q++) {
-		for (int w = 0; w < m; w++) {
+	char itable[n + 1][m + 1];
+	for (int q = 1; q <= n; q++) {
+		for (int w = 1; w <= m; w++) {
 			itable[q][w] = inf.readChar();
 		}
 		inf.readEoln();
@@ -50,11 +50,11 @@ int main(int argc, char* argv[]) {
 	}
 	pair<int, int> dir4[] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
-	char ptable[n][m] = {};
-	int prcnt[n] = {}, pccnt[m] = {}, tr, tc;
+	char ptable[n + 1][m + 1] = {};
+	int prcnt[n + 1] = {}, pccnt[m + 1] = {}, tr, tc;
 	set<pair<int, int>> tents;
 	string s;
-	for (int q = 0; q < n; q++) {
+	for (int q = 1; q <= n; q++) {
 		s = ouf.readString();
 		if (s == "QwQ") {
 			if (jans == "QwQ") {
@@ -64,16 +64,16 @@ int main(int argc, char* argv[]) {
 			}
 		}
 
-		for (int w = 0; w < m; w++) {
-			ptable[q][w] = s[w];
+		for (int w = 1; w <= m; w++) {
+			ptable[q][w] = s[w - 1];
 
 			if (itable[q][w] == 'T') {
 				if (ptable[q][w] != 'T') {
-					quitf(_wa, "(%d, %d) must be 'T', got '%c'.", q + 1, w + 1, ptable[q][w]);
+					quitf(_wa, "(%d, %d) must be 'T', got '%c'.", q, w, ptable[q][w]);
 				}
 			} else {
 				if (ptable[q][w] != '.' && ptable[q][w] != 'A') {
-					quitf(_wa, "(%d, %d) must be '.' or 'A', got '%c'.", q + 1, w + 1, ptable[q][w]);
+					quitf(_wa, "(%d, %d) must be '.' or 'A', got '%c'.", q, w, ptable[q][w]);
 				}
 			}
 
@@ -81,35 +81,35 @@ int main(int argc, char* argv[]) {
 				tents.insert({q, w});
 				prcnt[q]++;
 				pccnt[w]++;
-				if (q > 0 && itable[q - 1][w] == 'T') {
+				if (q > 1 && itable[q - 1][w] == 'T') {
 					// ok
-				} else if (q < n - 1 && itable[q + 1][w] == 'T') {
+				} else if (q < n && itable[q + 1][w] == 'T') {
 					// ok
-				} else if (w > 0 && itable[q][w - 1] == 'T') {
+				} else if (w > 1 && itable[q][w - 1] == 'T') {
 					// ok
-				} else if (w < m - 1 && itable[q][w + 1] == 'T') {
+				} else if (w < m && itable[q][w + 1] == 'T') {
 					// ok
 				} else {
-					quitf(_wa, "Tent (%d, %d) not connected to Tree.", q + 1, w + 1);
+					quitf(_wa, "Tent (%d, %d) not connected to Tree.", q, w);
 				}
 
 				for (int e = 0; e < 8; e++) {
 					tr = q + dir8[e].first;
 					tc = w + dir8[e].second;
-					if (tr >= 0 && tr < n && tc >= 0 && tc < m && ptable[tr][tc] == 'A') {
+					if (tr >= 1 && tr <= n && tc >= 1 && tc <= m && ptable[tr][tc] == 'A') {
 						quitf(_wa, "Tent (%d, %d) is close to tent (%d, %d).", q + 1, w + 1, tr + 1, tc + 1);
 					}
 				}
 			}
 		}
 	}
-	for (int q = 0; q < n; q++) {
+	for (int q = 1; q <= n; q++) {
 		if (rcnt[q] != prcnt[q]) {
 			quitf(_wa, "Count of tents on row %d incorrect.", q + 1);
 		}
 	}
 	int remtent = 0;
-	for (int q = 0; q < m; q++) {
+	for (int q = 1; q <= m; q++) {
 		if (ccnt[q] != pccnt[q]) {
 			quitf(_wa, "Count of tents on col %d incorrect.", q + 1);
 		}
@@ -126,7 +126,7 @@ int main(int argc, char* argv[]) {
 			for (int q = 0; q < 4; q++) {
 				tr = it->first + dir4[q].first;
 				tc = it->second + dir4[q].second;
-				if (tr >= 0 && tr < n && tc >= 0 && tc < m && ptable[tr][tc] == 'T') {
+				if (tr >= 1 && tr <= n && tc >= 1 && tc <= m && ptable[tr][tc] == 'T') {
 					pr = tr;
 					pc = tc;
 					cont++;
@@ -137,6 +137,7 @@ int main(int argc, char* argv[]) {
 				it = tents.erase(it);
 				remtent--;
 				changed = true;
+				solveCircle = false;
 			} else {
 				it++;
 			}
@@ -147,8 +148,6 @@ int main(int argc, char* argv[]) {
 			} else {
 				quitf(_wa, "Tent (%d, %d) connected to occupied tree", (*(tents.begin())).first, (*(tents.begin())).second);
 			}
-		} else {
-			solveCircle = false;
 		}
 	}
 
