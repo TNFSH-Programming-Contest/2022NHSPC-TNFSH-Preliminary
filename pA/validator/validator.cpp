9788e02eaf572f6887b828b16b215aacd0dcc202
Xi-Plus <huangxuanyuxiplus@gmail.com>
2022-09-11 19:58:31 +0800

    Fix link

--- pA/validator/validator.cpp
+++ pA/validator/validator.cpp
@@ -5,7 +5,7 @@ const int MAXN = 1000;
 
 int main() {
 	registerValidation();
-	// about testlib, see https://codeforces.com/blog/entry/18291
+	// about testlib, see https://codeforces.com/blog/entry/18426
 	inf.readEof();
 	return 0;
 }
--- pD/validator/validator.cpp
+++ pD/validator/validator.cpp
@@ -6,7 +6,7 @@ const int MAXN = 1000;
 
 int main() {
 	registerValidation();
-	// about testlib, see https://codeforces.com/blog/entry/18291
+	// about testlib, see https://codeforces.com/blog/entry/18426
 
 	int n, m;
 	n = inf.readInt();
